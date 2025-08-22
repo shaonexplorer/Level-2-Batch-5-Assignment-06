ccbe09f60567912feb302a3a0b1999e8186a2de0
shaonexplorer <shaonexplorer@gmail.com>
2025-08-22 18:05:15 +0600

    setup redux/RTK Query

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { RouterProvider } from "react-router";
 
 import { router } from "./router";
 import { ThemeProvider } from "./provider/theme.provider";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
 
 function App() {
   return (
     <>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <RouterProvider router={router}></RouterProvider>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <RouterProvider router={router}></RouterProvider>
+        </ThemeProvider>
+      </Provider>
     </>
   );
 }
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+  endpoints: (builder) => ({
+    getPokemonByName: builder.query({
+      query: (name) => `pokemon/${name}`,
+    }),
+  }),
+});
+
+export const { useGetPokemonByNameQuery } = baseApi;
